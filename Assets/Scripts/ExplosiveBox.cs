463a224b6b1430ee26ec418b545337af0367dd2e
mete han çetin <metehan_cetin43@hotmail.com>
2019-07-25 17:19:12 +0300

    ALLAH RIZASI IÇIN CALIS

--- Assets/Scripts/ExplosiveBox.cs
+++ Assets/Scripts/ExplosiveBox.cs
@@ -6,51 +6,50 @@ public class ExplosiveBox : BaseObstacle
 {
     public float radius = 5;
     public ParticleSystem boomPlosion;
+    public GameObject indicator;
 
     private GameObject basePlane;
     void Awake()
     {
-        basePlane=gameObject.transform.GetChild(0).gameObject;
+        basePlane = gameObject.transform.GetChild(0).gameObject;
         basePlane.transform.localScale = new Vector3(radius, 0.2f, radius);
-        basePlane.transform.parent=null;
+        basePlane.transform.parent = null;
     }
     void OnTriggerEnter(Collider collision)
     {
-        Debug.Log(collision.gameObject.name);
-        gameObject.GetComponent<SphereCollider>().enabled = false;
-        Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "Weapon" || collision.gameObject.tag == "Bomb")
         {
             performAction();
         }
     }
-
+    private Collider[] collided;
     private bool isExploded = false;
     public override void performAction()
     {
         if (!isExploded)
         {
-            Invoke("InvokeDestroy",0.25f);
+            Invoke("InvokeDestroy", 2f);
             isExploded = true;
             this.enabled = false;
-            Debug.Log("actionPerformed");
-            Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, radius);
-            foreach (Collider collider in hitColliders)
-            {
-                BaseObstacle temp = collider.gameObject.GetComponent<BaseObstacle>();
-                if (temp != null)
-                {
-                    temp.performAction();
-                }
-            }
+            collided = Physics.OverlapSphere(gameObject.transform.position, radius);
         }
     }
 
-    private void InvokeDestroy(){
+    private void InvokeDestroy()
+    {
+        indicator.GetComponent<MeshRenderer>().enabled = false;
         GameManagerScript.obstacleDestroyed();
-        basePlane.GetComponent<MeshRenderer>().enabled=false;
+        basePlane.GetComponent<MeshRenderer>().enabled = false;
         boomPlosion.Play();
-        gameObject.GetComponent<MeshRenderer>().enabled=false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        foreach (Collider collider in collided)
+        {
+            BaseObstacle temp = collider.gameObject.GetComponent<BaseObstacle>();
+            if (temp != null)
+            {
+                temp.performAction();
+            }
+        }
     }
 }
 
